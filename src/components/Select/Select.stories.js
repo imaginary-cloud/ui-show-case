df46381d84c2c311c10ba7ba2492433392421ba6
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-06-02 15:46:34 +0100

    feat(Select): add skeleton component

---
+++ src/components/Select/Select.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default {
+  title: 'Select',
+};
+
+export const Welcome = () => <h1>Select</h1>
---
+++ src/components/Select/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Button({ children }) {
+    return (
+        <Container>{children}</Container>
+    )
+}
+
+export default Button;

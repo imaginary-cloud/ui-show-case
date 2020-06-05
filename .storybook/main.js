200dcebb64d0c644078971a717c6038d25fac015
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-06-05 16:20:40 +0100

    chore: add addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-storysource',
     '@storybook/addon-docs',
-    '@storybook/addon-knobs/register'
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-a11y'
   ],
 };

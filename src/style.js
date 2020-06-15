d1067828acb797b9625580d93c8048d4db9bc546
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-06-15 17:23:54 +0100

    style: add font

--- src/style.js
+++ src/style.js
@@ -1,12 +1,16 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
   * {
-    font-family: 'sans-serif';
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 
     font-size: 16px;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
   }
 `

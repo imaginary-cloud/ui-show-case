090aadf67fcd0e4c0fc0fca28ed5dc21e705eca1
ronaiza-cardoso <ronaiza-cardoso@users.noreply.github.com>
2020-06-30 13:44:11 +0000

    deploy: aef3ea050bd5b2f4a56fe40f61def94f4e4cc261

---
+++ precache-manifest.b2ec3ba50b4a73fbccdc399187a861c2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570459148c61e1041fda71059333af6c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2a2e744a3101f2928adf.bundle.js"
+  },
+  {
+    "url": "runtime~main.2a2e744a3101f2928adf.bundle.js"
+  },
+  {
+    "url": "vendors~main.2a2e744a3101f2928adf.bundle.js"
+  },
+  {
+    "url": "vendors~main.2a2e744a3101f2928adf.bundle.js.LICENSE.txt"
+  }
+]);

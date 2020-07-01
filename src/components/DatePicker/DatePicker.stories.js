106e44020178f786789347d2ea30bfa0a309b9ab
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-07-01 16:08:32 +0100

    chore: remove dumb container and add storybook decorator

--- src/components/DatePicker/DatePicker.stories.js
+++ src/components/DatePicker/DatePicker.stories.js
@@ -3,44 +3,24 @@ import { text } from '@storybook/addon-knobs'
 
 import DatePicker from './index'
 
-import Container from '../Container'
-
 export default {
   title: 'Components/DatePicker',
 }
 
 export const DatePickerDefault = () => (
-  <Container
-    style={{
-      marginTop: '10px',
-      height: '100%',
-      width: '60%',
-      margin: '20% auto',
-    }}
-  >
-    <DatePicker
-      label={text('label', 'Date')}
-      value=""
-      onDateChanged={(d) => console.log('---> DatePicker ---> ', d)}
-    />
-  </Container>
+  <DatePicker
+    label={text('label', 'Date')}
+    value=""
+    onDateChanged={(d) => console.log('---> DatePicker ---> ', d)}
+  />
 )
 
 export const DatePickerWithRange = () => (
-  <Container
-    style={{
-      marginTop: '10px',
-      height: '100%',
-      width: '60%',
-      margin: '20% auto',
-    }}
-  >
-    <DatePicker
-      label={text('label', 'Date')}
-      isRange
-      placeholder="DD/MM/YYYY - DD/MM/YYYY"
-      value=""
-      onDateChanged={(d) => console.log('---> DatePicker ---> ', d)}
-    />
-  </Container>
+  <DatePicker
+    label={text('label', 'Date')}
+    isRange
+    placeholder="DD/MM/YYYY - DD/MM/YYYY"
+    value=""
+    onDateChanged={(d) => console.log('---> DatePicker ---> ', d)}
+  />
 )
